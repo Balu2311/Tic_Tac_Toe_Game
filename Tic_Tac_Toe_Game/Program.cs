8fb99019fdb8f29805512baf0229f499d31908f7
Vallem Balu <vbnreddy5@gmail.com>
2021-08-05 10:44:14 +0530

    Blank Game Board To Play Tic Tac Toe

---
+++ Tic_Tac_Toe_Game/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tic_Tac_Toe_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacTone Game");
+            TicTacToeGame obj = new TicTacToeGame();
+            obj.gameBoard();
+        }
+    }
+}
---
+++ Tic_Tac_Toe_Game/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe_Game
+{
+    class TicTacToeGame
+    {
+        char[] Board = new char[10];
+        public void gameBoard()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Board[i] = ' ';
+            }
+        }
+    }
+}
